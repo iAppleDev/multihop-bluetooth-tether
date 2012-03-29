54c6fcc523d3c9a9aad8569ca94e4b1a531027d9
sarun87@gmail.com <sarun87@gmail.com@11c7d636-0986-e9d9-50a6-bae780321c72>
2012-03-29 09:09:53 +0000

    Added code to work with 3 mobiles. Single hop

--- trunk/MHI/src/ncsu/wireless/MHIActivity.java
+++ trunk/MHI/src/ncsu/wireless/MHIActivity.java
@@ -4,27 +4,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MHIActivity extends Activity {
+
 	private static final int REQUEST_ENABLE_BT = 1;
+	public HashMap<String, String> MACList;
+	public HashMap<String, HashMap<String, String>> macIPcollection_client;
+	
+	private String myAddress = "";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        MACList = new HashMap<String, String>();
+        
+        populateHardCodedList();
+        /// Switch on bluetooth device
 		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
 				.getDefaultAdapter();
+		myAddress = mBluetoothAdapter.getAddress();
 		// to enable bluetooth
 		if (!mBluetoothAdapter.isEnabled()) {
 			Intent enableBtIntent = new Intent(
@@ -32,9 +48,46 @@ public class MHIActivity extends Activity {
 			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}
     }
-    public void onExitClicked(){
+
+	private void populateHardCodedList() {
+		macIPcollection_client = new HashMap<String, HashMap<String, String>>();
+		
+		
+//		HashMap<String, String> tempMap1 = new HashMap<String, String>();
+//		tempMap1.put("D8:54:3A:08:42:3F", "10.0.12.2");
+//		tempMap1.put("F4:FC:32:7E:35:9D", "10.0.13.2");
+//		macIPcollection_client.put("F4:FC:32:72:DA:EE", tempMap1);
+//
+//		HashMap<String, String> tempMap2 = new HashMap<String, String>();
+//		tempMap2.put("F4:FC:32:72:DA:EE", "10.0.12.2");
+//		tempMap2.put("F4:FC:32:7E:35:9D", "10.0.23.2");
+//		macIPcollection_client.put("D8:54:3A:08:42:3F", tempMap2);
+//
+//		HashMap<String, String> tempMap3 = new HashMap<String, String>();
+//		tempMap3.put("F4:FC:32:72:DA:EE", "10.0.13.2");
+//		tempMap3.put("D8:54:3A:08:42:3F", "10.0.23.2");
+//		macIPcollection_client.put("F4:FC:32:7E:35:9D", tempMap3);
+			
+		
+		HashMap<String, String> tempMap1 = new HashMap<String, String>();
+		tempMap1.put("D8:54:3A:08:42:3F", "10.0.12.2");
+		tempMap1.put("F4:FC:32:7E:CF:15", "10.0.13.2");
+		macIPcollection_client.put("F4:FC:32:4F:0D:D6", tempMap1);
+
+		HashMap<String, String> tempMap2 = new HashMap<String, String>();
+		tempMap2.put("F4:FC:32:4F:0D:D6", "10.0.12.2");
+		tempMap2.put("F4:FC:32:7E:CF:15", "10.0.23.2");
+		macIPcollection_client.put("D8:54:3A:08:42:3F", tempMap2);
+
+		HashMap<String, String> tempMap3 = new HashMap<String, String>();
+		tempMap3.put("F4:FC:32:4F:0D:D6", "10.0.13.2");
+		tempMap3.put("D8:54:3A:08:42:3F", "10.0.23.2");
+		macIPcollection_client.put("F4:FC:32:7E:CF:15", tempMap3);
+	}
+	
+	public void onExitClicked(View v){
     	 finish();
-    	System.exit(0);
+    	//System.exit(0);
     }
     public String execCommandLine(String command) {
 		Runtime runtime = Runtime.getRuntime();
@@ -115,19 +168,85 @@ public class MHIActivity extends Activity {
 
 		} 
 	}
-	public void onClientClicked(View v){
+	public void onClientClicked(View v) throws InterruptedException{
 		// CLIENT CODE
 		if(((ToggleButton )v).isChecked()){
-				Toast.makeText(MHIActivity.this, "Off", Toast.LENGTH_SHORT)
+				execCommandLine("pand --killall");
+				execCommandLine("killall -9 pand");
+				Toast.makeText(MHIActivity.this, "Client Mode On", Toast.LENGTH_SHORT)
 						.show();
 
-				execCommandLine("pand --connect f4:fc:32:4f:0d:d6");
-				String capture = execCommandLine("/system/bin/netcfg");
-
-				execCommandLine("ifconfig bnep0 10.0.1.2 netmask 255.255.255.0 up");
-				execCommandLine("route add default gw 10.0.1.1 dev bnep0");
-				execCommandLine("setprop net.dns1 10.0.1.1");
+				// Populate bluetooth device mac addresses
+				populatePairedBTDeviceMACs();
+				
+				//printMACMap();
+				String serverMAC = findServer();
+				HashMap<String,String> level1Map = (macIPcollection_client.get(myAddress));
+				if(level1Map == null){
+					return;
+				}
+				String myIP = level1Map.get(serverMAC);
+				if(myIP == null){
+					return;
+				}
+				String myGateway = (String) myIP.subSequence(0, myIP.length()-1);
+				myGateway = myGateway.concat("1");
+				execCommandLine("ifconfig bnep0 "+myIP+" netmask 255.255.255.0 up");
+				execCommandLine("route add default gw "+myGateway+" dev bnep0");
+				execCommandLine("setprop net.dns1 "+myGateway);
 				execCommandLine("echo 1 > /proc/sys/net/ipv4/ip_forward");
 		}
+	}
+	
+	private String findServer() throws InterruptedException {
+		String returnValue="";
+		Iterator<Entry<String, String>> it = MACList.entrySet().iterator();
+		while(it.hasNext()){
+			 Entry<String, String> pairs = (Entry<String, String>)it.next();
+			 String MAC = pairs.getValue();
+			 execCommandLine("pand --connect "+MAC);
+			 Thread.sleep(2000);
+			 String capture = execCommandLine("/system/bin/netcfg");
+		     if(capture.contains("bnep0") == true) {
+				 returnValue = MAC;
+				 break;
+			 }
+		}
+		// Found MAC
+		return returnValue;
+	}
+	private void printMACMap() {
+		Iterator<Entry<String, String>> it = MACList.entrySet().iterator();
+		String text="";
+		while(it.hasNext()){
+			 Entry<String, String> pairs = (Entry<String, String>)it.next();
+			 text+="Device:"+pairs.getKey() + "\n" + "MAC:"+pairs.getValue()+"\n";
+		}
+		TextView tv = new TextView(this);
+        tv.setText(text);
+        setContentView(tv);
+	}
+	
+	private void populatePairedBTDeviceMACs() {
+		 BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	        if (mBluetoothAdapter == null) {
+	            // Device does not support Bluetooth
+	        }
+	        if (!mBluetoothAdapter.isEnabled()) {
+	            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+	        }
+	        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+	        //String devName =  mBluetoothAdapter.getName()+"\n";
+	     // If there are paired devices
+	     if (pairedDevices.size() > 0) {
+	         // Loop through paired devices
+	         for (BluetoothDevice device : pairedDevices) {
+				// Add the name and address to an array adapter to show in a ListView
+	        	 String devName = device.getName();
+	        	 String devMAC = device.getAddress();
+	        	 MACList.put(devName, devMAC);
+	         }
+	     }
 	}
 }
